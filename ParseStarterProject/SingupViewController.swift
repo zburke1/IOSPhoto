3df3662ace69d05881624097e2148c4b5db99f3b
Zac <zburke1@ufl.edu>
2015-11-08 18:13:28 -0500

    Added more tweaks

--- ParseStarterProject/SingupViewController.swift
+++ ParseStarterProject/SingupViewController.swift
@@ -22,41 +22,81 @@ class SingupViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    @IBAction func signupFunc(sender: AnyObject) {
-        if userNameText.text != "" && emailText.text != "" && pwdText.text != "" {
-            let user = PFUser()
-            user.username = userNameText.text
-            user.password = pwdText.text
-            user.email = emailText.text
-            
-            user.signUpInBackgroundWithBlock {
-                (succeeded: Bool, error: NSError?) -> Void in
-                if let error = error {
-                    let errorString = error.userInfo["error"] as? NSString
-                    print(errorString)
-                    if(error.code == 202){
-                        let alertView = UIAlertView(title: "Oops!", message: "That username is taken", delegate: nil, cancelButtonTitle: "OK")
-                        alertView.show()
-                    }
-                    else if(error.code == 203){
-                        let alertView = UIAlertView(title: "Oops!", message: "That email is already taken", delegate: nil, cancelButtonTitle: "OK")
-                        alertView.show()
-                    }
-                } else {
-                    print("You have succesfully signed up! Now logging in")
-                   // self.loginFunc()
-                }
-            }
-            
-            
-        }
-        else{
-            let alertView = UIAlertView(title: "Oops!", message: "Please fill in all fields before submitting", delegate: nil, cancelButtonTitle: "OK")
-            alertView.show()
-        }
     
+    @IBAction func signupGo(sender: AnyObject) {
+                if userNameText.text != "" && emailText.text != "" && pwdText.text != "" {
+                    let user = PFUser()
+                    user.username = userNameText.text
+                    user.password = pwdText.text
+                    user.email = emailText.text
+        
+                    user.signUpInBackgroundWithBlock {
+                        (succeeded: Bool, error: NSError?) -> Void in
+                        if let error = error {
+                            let errorString = error.userInfo["error"] as? NSString
+                            print(errorString)
+                            if(error.code == 202){
+                                let alertView = UIAlertView(title: "Oops!", message: "That username is taken", delegate: nil, cancelButtonTitle: "OK")
+                                alertView.show()
+                            }
+                            else if(error.code == 203){
+                                let alertView = UIAlertView(title: "Oops!", message: "That email is already taken", delegate: nil, cancelButtonTitle: "OK")
+                                alertView.show()
+                            }
+                        } else {
+                            print("You have succesfully signed up! Now logging in")
+                           // self.loginFunc()
+                        }
+                    }
+        
+        
+                }
+                else{
+                    let alertView = UIAlertView(title: "Oops!", message: "Please fill in all fields before submitting", delegate: nil, cancelButtonTitle: "OK")
+                    alertView.show()
+                }
+            
+        
     
     }
+    
+    
+    
+//    @IBAction func signupFunc(sender: AnyObject) {
+//        if userNameText.text != "" && emailText.text != "" && pwdText.text != "" {
+//            let user = PFUser()
+//            user.username = userNameText.text
+//            user.password = pwdText.text
+//            user.email = emailText.text
+//            
+//            user.signUpInBackgroundWithBlock {
+//                (succeeded: Bool, error: NSError?) -> Void in
+//                if let error = error {
+//                    let errorString = error.userInfo["error"] as? NSString
+//                    print(errorString)
+//                    if(error.code == 202){
+//                        let alertView = UIAlertView(title: "Oops!", message: "That username is taken", delegate: nil, cancelButtonTitle: "OK")
+//                        alertView.show()
+//                    }
+//                    else if(error.code == 203){
+//                        let alertView = UIAlertView(title: "Oops!", message: "That email is already taken", delegate: nil, cancelButtonTitle: "OK")
+//                        alertView.show()
+//                    }
+//                } else {
+//                    print("You have succesfully signed up! Now logging in")
+//                   // self.loginFunc()
+//                }
+//            }
+//            
+//            
+//        }
+//        else{
+//            let alertView = UIAlertView(title: "Oops!", message: "Please fill in all fields before submitting", delegate: nil, cancelButtonTitle: "OK")
+//            alertView.show()
+//        }
+//    
+//    
+//    }
     
     
     override func didReceiveMemoryWarning() {
