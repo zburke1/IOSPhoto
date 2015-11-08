d81b04559fe6827901d7574a0ae050e9389e6432
Zac <zburke1@ufl.edu>
2015-11-08 17:09:43 -0500

    initial setup

--- ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ViewController.swift
@@ -12,13 +12,71 @@ import Parse
 
 class ViewController: UIViewController {
 
+    @IBOutlet var usernameText: UITextField!
+    @IBOutlet var passwordText: UITextField!
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func loginGo(sender: AnyObject) {
+            if usernameText.text !=  "" && passwordText.text != "" {
+                PFUser.logInWithUsernameInBackground(usernameText.text!, password:passwordText.text!) {
+                    (user: PFUser?, error: NSError?) -> Void in
+                    if user != nil
+                    {
+                        // Do stuff after successful login.
+                        print("You logged in")
+                        self.performSegueWithIdentifier("mainLoginSegue", sender: self)
+        
+                    }
+                    else
+                    {
+                        if(error?.code==101){
+                            let alertView = UIAlertView(title: "Oops!", message: "Your user/password is incorrect", delegate: nil, cancelButtonTitle: "OK")
+                            alertView.show()
+                            print(error?.description)
+                        }
+                    }
+                }
+                
+                
+            }
+    
+    
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 }
+
+
+
+//@IBAction func loginGo(sender: AnyObject) {
+//    if usernameText.text !=  "" && passwordText.text != "" {
+//        PFUser.logInWithUsernameInBackground(usernameText.text!, password:passwordText.text!) {
+//            (user: PFUser?, error: NSError?) -> Void in
+//            if user != nil
+//            {
+//                // Do stuff after successful login.
+//                print("You logged in")
+//                self.performSegueWithIdentifier("mainLoginSegue", sender: self)
+//                
+//            }
+//            else
+//            {
+//                if(error?.code==101){
+//                    let alertView = UIAlertView(title: "Oops!", message: "Your user/password is incorrect", delegate: nil, cancelButtonTitle: "OK")
+//                    alertView.show()
+//                    print(error?.description)
+//                }
+//            }
+//        }
+//        
+//        
+//    }
